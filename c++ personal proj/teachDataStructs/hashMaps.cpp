21cd38ef6b1968b038ba73cb9856c75eb86f68bc
Sean Dumont <Seandumont2005@gmail.com>
2025-02-14 14:20:23 -0500

    updated hashmaps

--- c++ personal proj/teachDataStructs/hashMaps.cpp
+++ c++ personal proj/teachDataStructs/hashMaps.cpp
@@ -12,8 +12,8 @@ private:
 public:
     bool isEmpty() const;
     int hashFunction(int key);
-    void insertItem(int key, std::string value);
-    void removeItem(int key);
+    bool insertItem(int key, std::string value);
+    bool removeItem(int key,std::string value);
     std::string searchTable(int key);
     void printTable();
 };
@@ -36,9 +36,46 @@ int HashTable::hashFunction(int key) {
 }
 
 
-void HashTable::insertItem(int key, std::string value) {
-    int hashValue = hashFunction(key);// Find hash value of whatever ur key is so it can later be found inside the hash table
-    std::list<std::pair<int,std::string>>& hashTablecell = table[hashValue]; //now going inside ur hashtable with the hashed key locate the space where the hashed key is.
-    std::list<std::pair<int,std::string>> BucketbeginIterator = hashTablecell.begin();// start 
-    bool keyExists = false;
+bool HashTable::insertItem(int key, std::string value) {
+    int bucketHashValue = hashFunction(key);// Find hash value of whatever ur key is so it can later be found inside the hash table
+    std::list<std::pair<int,std::string>>& bucket = table[bucketHashValue]; //now going inside ur hashtable with the hashed key locate the space where the hashed key is.
+    // We'll iterate through the list to find an existing key so we need a flag to let us know if we found it or not.
+
+    //declare the type of iterator is as u can just use a normal for(int i - 0) remeber it says ::iterator
+    std::list<std::pair<int, std::string>>::iterator hashTableIterator;
+    //the way we iterate it is we check to see if its the end of the hashtable or the null part and if it isnt than iterate 
+    for(hashTableIterator = bucket.begin();hashTableIterator != bucket.end();hashTableIterator++){
+        if (hashTableIterator->first == key) { //if the iterator first part of the pair equals the key then
+            hashTableIterator->second = value;  // Update value
+            return false;        // Return false to indicate update (not insertion)
+        }
+    }
+    //Scenario	                                            Will it Update?	Will it Add a New Entry?
+    //Insert new key	                                     ❌ No	✅ Yes
+    //Insert same key again	                                ✅ Yes	❌ No
+    //Two different keys, same hash (collision)	            ❌ No	✅ Yes
+    // If we reach this point, the key was not found, so insert a new one
+    bucket.push_back(std::make_pair(key, value));
+    return true;  // A new key-value pair was added
 }
+
+bool HashTable::removeItem(int key,std::string value){
+    int bucketHashValue = hashFunction(key);
+    std::list<std::pair<int,std::string>>& bucket = table[bucketHashValue];
+
+    //make itterator for forloop
+    std::list<std::pair<int, std::string>>::iterator hashTableIterator;
+    for(hashTableIterator = bucket.begin();hashTableIterator != bucket.end();hashTableIterator++){
+        if(hashTableIterator->first == key && hashTableIterator->second == value){
+            bucket.erase(hashTableIterator);
+            std::cout<<" deleted succesfully" << "\n";
+            return true;
+        }
+    }
+    std::cout << "Key not found!" << "\n";
+    return false;  // Return false if the key-value pair was not found
+
+
+
+}
+
